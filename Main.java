f657eb7c2248f287ee513c2a695d52956da7e5d5
Muhammad Conn <126736146+pycoder08@users.noreply.github.com>
2025-02-15 12:32:29 -0800

    Update Main.java
    We use integer division, modulo operators, and formatted output to evaluate the coin values of a given dollar amount.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-// COLLABORATORS: <tutors, instructor, or anyone else who helped you>
+// Student Name: Muhammad Conn
 
-// DESCRIPTION: <a sentence about what the purpose of this program is>
+// DESCRIPTION:  This is a program that converts a dollar amount to quarters, dimes, nickels, and pennies.
 
 public class Main
 {
@@ -8,10 +8,39 @@ public class Main
 	{
     //DECLARATION + INITIALIZATION SECTION
 
+    //We define the input dollar value
+    double dollarAmount = 1.16; 
+
+    //We define a second dollar value, multiplying by 100 temporarily as well as using the round operation to avoid Java rounding errors
+    int bigDollarAmount = (int) Math.round(100 * dollarAmount); 
+
+    //Variables for each coin amount
+    int quarters, dimes, nickels, pennies;
+
+    //Variables for the remainder after each coin value
+    int leftAfterQuarters;
+    int leftAfterDimes;
+    int leftAfterNickels;
     //CALCULATION SECTION
 
+    //We use integer division to see how  many of each coin go into the dollar amount. We multiply the coin value by 100 to account for our previous operation to avoid rounding errors
+    quarters = (bigDollarAmount / 25);
+    leftAfterQuarters = bigDollarAmount % 25;
+    
+    dimes = leftAfterQuarters / 10;
+    leftAfterDimes = leftAfterQuarters % 10;
+
+    nickels = leftAfterDimes / 5;
+    leftAfterNickels = leftAfterDimes % 5;
+
+    pennies = leftAfterNickels;
+
     //OUTPUT SECTION
+    System.out.println(quarters);
+    System.out.println(dimes);
+    System.out.println(nickels);
+    System.out.println(pennies);
 
-  }
-
+    System.out.printf("$%.2f can be converted to %d quarters, %d dimes, %d nickels, and %d pennies.", dollarAmount, quarters, dimes, nickels, pennies);
+    }
 }
